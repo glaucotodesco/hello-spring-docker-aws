d97d163c486a8ef2a237f34535a9719842567246
glauco.todesco <glauco.todesco@facens.br>
2020-12-02 00:24:49 -0300

    Exemplo Basico

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DemoController {
+
+
+    @GetMapping
+    public String getHello()
+    {
+        return "Hello Spring and Docker";
+    }
+    
+}
